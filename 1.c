0a97ee8ab6e13e2fa0f6df6f2e719a06d6a95ee9
Shaman <shamanhussain@gmail.com>
2021-05-03 01:04:01 +0530

    'add'

---
+++ 1.c
@@ -0,0 +1,13 @@
+//Type declaration instructions example program
+
+#include <stdio.h>
+int main(void)
+{
+    // int a = 4; //Type declaration instruction (declaring same value twice will show error)
+    int a = 5, c =1 ; //Type declaration instruction
+    float b = a + 5.56; //Type declaration instruction
+    printf("The value of a is %d \n", a);
+    printf("The value of b is %f \n", b);
+    printf("The value of c is %d \n", c);
+    
+}
